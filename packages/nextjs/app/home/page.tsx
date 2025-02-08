1e0ae345718fb13acf301da676a40662f0ba73af
Aisari Amirov <aisari.amirov1@gmail.com>
2025-02-08 13:02:40 +0000

    IPFS Check Compnents

--- packages/nextjs/app/home/page.tsx
+++ packages/nextjs/app/home/page.tsx
@@ -9,20 +9,32 @@ import { Address } from "~~/components/scaffold-eth";
 import SectionOneMointains from "~~/components/ByAisari/SectionOneMointains";
 import PdfUploader from "~~/components/ByDanylo/PDFUpload";
 import CardsExplanation from "~~/components/ByAisari/CardsExplanation";
+import IPFSCheck from "~~/components/ByAisari/IPFSCheck";
 
+const fakeJsonData = {
+  message: "Hello, IPFS!",
+};
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
 
   return (
     <>
+      <SectionOneMointains />
+      <CardsExplanation />
+      <PdfUploader />
+      <IPFSCheck jsonData={fakeJsonData} />
 
+<<<<<<< Updated upstream
     {/*<SectionOneMointains />*/}
     {/*<CardsExplanation />*/}
     <PdfUploader />
 
     
     <div className="flex items-center flex-col flex-grow pt-10">
+=======
+      <div className="flex items-center flex-col flex-grow pt-10">
+>>>>>>> Stashed changes
         {/* If you still want to show landingVideo, just keep it here or remove it */}
         {/* <landingVideo /> */}
         {/* The rest of your page content */}
