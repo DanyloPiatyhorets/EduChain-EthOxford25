8058ffeb5f4781e688f0352501e259b4f8adb2e9
Danylo Piatyhorets <Danylo.Piatyhorets@city.ac.uk>
2025-02-09 12:59:11 +0000

    updated zk page

--- packages/nextjs/app/zkproofs/page.tsx
+++ packages/nextjs/app/zkproofs/page.tsx
@@ -6,6 +6,9 @@ import ZkAppComponent from "~~/components/ByAisari/zkappproofs";
 const Home: NextPage = () => {
   return (
     <>
+      <div className={"text-center"}>check zk-app deployed contract address through MINA: B62qj7bcdKCK9YkthAt1nB4VwRSKutpZWiZCdT9t5u3ewisidSdzjuB</div>
+      <div className={"text-center"}>check on minascan.io</div>
+      <div className={"text-center"}>Transaction hash: 5JuhttjB41WYpnSG9qWAz4yiWAvSecMPThg3FdMkempwn7FUp8pv</div>
     <ZkAppComponent />
     </>
   );
